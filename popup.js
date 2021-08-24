16602dd1b36d8bc1596e9409a7e01d4200f294b5
Aurelissima <hajnalka.molnar.0510@gmail.com>
2021-08-24 14:51:50 +0200

    feat(popup): collapsibles

--- popup.js
+++ popup.js
@@ -1,24 +1,20 @@
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+var coll = document.getElementsByClassName("name");
+var i;
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
   });
-});
+}
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
+toggleDetails = (event) => {
+  console.log({ event })
 }
