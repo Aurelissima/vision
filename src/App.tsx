6c04794ba46f505ad4d44b6980d5a76903993bba
Aurelissima <hajnalka.molnar.0510@gmail.com>
2021-09-10 16:11:55 +0200

    feat(*): add typescript and react
    Co-authored-by: KaposiOli <KaposiOli@users.noreply.github.com>

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import './App.css';
+import './popup/popup.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body className="body">
+  <div className="container">
+    <div className="tournaments">
+      <div>Tournaments</div>
+      <button className="addButton">Add More</button>
+    </div>
+    <div className="collapsible">
+      <div className="name">LEC</div>
+      <div className="details">Kek</div>
+    </div>
+    <div className="collapsible">
+      <div className="name">LCK</div>
+      <div className="details">Kek</div>
+    </div>
+    <div className="collapsible">
+      <div className="name">LCS</div>
+      <div className="details">Kek</div>
+    </div>
+    <div className="collapsible">
+      <div className="name">LCL</div>
+      <div className="details">Kek</div>
+    </div>
+    <div className="collapsible">
+      <div className="name">LPL</div>
+      <div className="details">Kek</div>
+    </div>
+    <div className="tournaments">
+      <div>Teams</div>
+      <button className="addButton">Add More</button>
+    </div>
+    <div className="collapsible">
+      <div className="name">TSM</div>
+      <div className="details">Kek</div>
+    </div>
+    <div className="collapsible">
+      <div className="name">G2</div>
+      <div className="details">Kek</div>
+    </div>
+    <div className="collapsible">
+      <div className="name">T1</div>
+      <div className="details">Kek</div>
+    </div>
+    <div className="collapsible">
+      <div className="name">FNC</div>
+      <div className="details">Kek</div>
+    </div>
+    <div className="collapsible">
+      <div className="name">C9</div>
+      <div className="details">Kek</div>
+    </div>
+  </div>
+  <script src="popup.js"></script>
+</body>
+    </div>
+  );
+}
+
+export default App;
